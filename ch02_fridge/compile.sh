fd2049c1c3d53c5c3dcd748cf593ef3b24e9a83d
Víctor R. Escobar <victor.r.e.o@gmail.com>
2015-02-22 12:07:22 +0100

    Added CPP target to the compilation

--- ch02_fridge/compile.sh
+++ ch02_fridge/compile.sh
@@ -1,2 +1,8 @@
 mkdir -p build/
 haxe -cp src/ -main myfridge.MyFridge -neko build/fridge.n
+haxe -cp src/ -main myfridge.MyFridge -cpp build/fridge.cpp
+
+# Open
+# neko build/fridge.n
+# or for CPP
+# ./build/fridge.cpp/MyFridge
